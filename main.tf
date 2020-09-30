e0dfd7f2c5d47bdc8b60f6b33f31cb1d5581ebb5
Callum <callumccr@gmail.com>
2020-09-30 16:13:09 +0100

    updated enabled arg

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "default" {
-  count         = module.label.enabled ? 1 : 0
+  count         = var.enabled ? 1 : 0
   bucket        = module.label.id
   acl           = var.acl
   force_destroy = var.force_destroy
@@ -75,7 +75,7 @@ resource "aws_s3_bucket" "default" {
 # https://www.terraform.io/docs/providers/aws/r/s3_bucket_public_access_block.html
 # for the nuances of the blocking options
 resource "aws_s3_bucket_public_access_block" "default" {
-  count  = module.label.enabled ? 1 : 0
+  count  = var.enabled ? 1 : 0
   bucket = join("", aws_s3_bucket.default.*.id)
 
   block_public_acls       = var.block_public_acls
